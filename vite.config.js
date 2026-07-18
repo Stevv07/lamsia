f179575cf8261298930bc00ea9a2f28d38debf04
Stevv07 <samosirmarcell103@gmail.com>
2026-07-18 19:32:27 +0700

    updating vite

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     react(),
     babel({ presets: [reactCompilerPreset()] }),
     tailwindcss(),
-  ],
+  ],  
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
